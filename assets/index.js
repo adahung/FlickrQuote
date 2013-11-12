bc0aaee304a89247822db4da1e82e481a68ab20f
adahung <adahung@yahoo-inc.com>
2013-11-13 01:11:35 +0800

    Add http request to get flickr photos from api

--- assets/index.js
+++ assets/index.js
@@ -11,10 +11,56 @@ function getSelectedText() {
 function doSomethingWithSelectedText() {
     var selectedText = getSelectedText();
     if (selectedText) {
-        alert("Got selected text " + selectedText);
+        document.getElementById('text').innerHTML = 'Search <b>' + selectedText + '</b> in FlickrCentral (34427469792@N01)';
+        getPhotosFromFlickr(selectedText, '34427469792@N01');
     }
 }
 
+function getPhotosFromFlickr(text, group_id) {
+    var url = 'http://nameless-fjord-2265.herokuapp.com/photo',
+        args = 'text=' + encodeURIComponent(text) + '&group_id=' + encodeURIComponent(group_id) + '&sort=interestingness-desc';
+    makeRequest(url + '?' + args, parseResp);
+}
+
+function parseResp(req) {
+    console.log(req.responseText.substring(0, 10));
+}
+
+function makeRequest(url, callback) {
+    var httpRequest;
+    console.log(url);
+    if (window.XMLHttpRequest) { // Mozilla, Safari, ...
+      httpRequest = new XMLHttpRequest();
+    } else if (window.ActiveXObject) { // IE
+      try {
+        httpRequest = new ActiveXObject("Msxml2.XMLHTTP");
+      } 
+      catch (e) {
+        try {
+          httpRequest = new ActiveXObject("Microsoft.XMLHTTP");
+        } 
+        catch (e) {}
+      }
+    }
+
+    if (!httpRequest) {
+      console.log('Giving up :( Cannot create an XMLHTTP instance');
+      return false;
+    }
+    httpRequest.open('GET', url);
+    // http://www.quirksmode.org/js/xmlhttp.html
+    httpRequest.onreadystatechange = function () {
+        if (httpRequest.readyState == 4) {
+            if (httpRequest.status == 200 || httpRequest.status == 304) {
+                callback(httpRequest);
+            }
+        }
+    }
+    if (httpRequest.readyState == 4) return;
+    httpRequest.send();
+}
+
+
 var q = document.getElementById("quote");
-console.log(q);
 q.onmouseup = doSomethingWithSelectedText;
+
