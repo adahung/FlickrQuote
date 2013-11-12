45b7206ab1f9c51791bdf2aabb02a6919a46760e
adahung <adahung@yahoo-inc.com>
2013-11-12 22:41:06 +0800

    Initial commit for FlickrQuote

---
+++ assets/index.js
@@ -0,0 +1,20 @@
+function getSelectedText() {
+    var text = "";
+    if (typeof window.getSelection != "undefined") {
+        text = window.getSelection().toString();
+    } else if (typeof document.selection != "undefined" && document.selection.type == "Text") {
+        text = document.selection.createRange().text;
+    }
+    return text;
+}
+
+function doSomethingWithSelectedText() {
+    var selectedText = getSelectedText();
+    if (selectedText) {
+        alert("Got selected text " + selectedText);
+    }
+}
+
+var q = document.getElementById("quote");
+console.log(q);
+q.onmouseup = doSomethingWithSelectedText;
