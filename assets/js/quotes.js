fbd868d403d91f50e537e7c23da45894019a09d7
adahung <adahung@yahoo-inc.com>
2013-11-15 22:10:34 +0800

    prepare demo

--- assets/js/quotes.js
+++ assets/js/quotes.js
@@ -1,19 +1,20 @@
 function Quotes() {
 	this.quotes = [
 		"The only way to do great work is to love what you do. -Steve Jobs",
-		"lalala -Ada",
 		"Whatever the mind of man can conceive and believe, it can achieve. -Napoleon Hill",
 		"Your time is limited, so don't waste it living someone else's life. -Steve Jobs",
         "Strive not to be a success, but rather to be of value. -Albert Einstein",
-        "Two roads diverged in a wood, and I took the one less traveled by, and that has made all the difference.  -Robert Frost",
-		"The common question that gets asked in business is, 'why?' That’s a good question, but an equally valid question is, 'why not?' -Jeffrey Bezos",
+        "Two roads diverged in a wood, and I took the one less traveled by, and that has made all the difference. -Robert Frost",
 		"You miss 100% of the shots you don't take. -Wayne Gretzky",
-		"I’ve missed more than 9000 shots in my career. I’ve lost almost 300 games. 26 times I've been trusted to take the game winning shot and missed. I’ve failed over and over and over again in my life. And that is why I succeed. -Michael Jordan",
+		"Change your thoughts and you change your world. -Norman Vincent Peale",
+		"Life is what we make it, always has been, always will be. -Grandma Moses",
+		'Nothing is impossible, the word itself says, "I\'m possible!" -Audrey Hepburn'
 	];
 
 	this.getOne = function() {
-		var index = Math.floor((Math.random()*this.getQuoteLength()));
-		return this.quotes[index];
+		//var index = Math.floor((Math.random()*this.getQuoteLength()));
+		//return this.quotes[index];
+		return '';
 	}
 
 	this.getQuoteLength = function() {
@@ -35,8 +36,8 @@ function PhotoTemplates() {
 function FlickrGroup() {
 	this.groups = [
 		{ id: '34427469792@N01', name: 'FlickrCentral'},
-		{ id: '1463451@N25', name: 'Project Weather'},
-		{ id: '35034344814@N01', name: 'Dogs! Dogs! Dogs!'} //FlickrCentral (34427469792@N01)
+		{ id: '1463451@N25', name: 'Project Weather'}//,
+		//{ id: '35034344814@N01', name: 'Dogs! Dogs! Dogs!'} //FlickrCentral (34427469792@N01)
 	];
 
 	this.getRandomGroup = function() {
@@ -48,12 +49,12 @@ function FlickrGroup() {
 function FlickrParams() {
 	this.sort = [
 		//'date-posted-asc', 
-		'date-posted-desc', 
+		//'date-posted-desc', 
 		//'date-taken-asc', 
-		'date-taken-desc', 
-		'interestingness-desc', 
+		//'date-taken-desc', 
+		'interestingness-desc'//, 
 		//'interestingness-asc', 
-		'relevance'
+		//'relevance'
 	];
 
 	this.getRandomSort = function() {
